b4e8effd58d0e75e5cb318d65428ad59addd0dcc
bensoer <bsoer@my.bcit.ca>
2015-02-11 18:04:15 -0800

    filled in models. mostly guesses

---
+++ GoodSamaritan/Models/ClientModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoodSamaritan.Models
+{
+    public class ClientModel
+    {
+        public int ClientReferenceNumber { get; set; }
+
+        public List<FiscalYearModel> FiscalYear { get; set; }
+
+        public int Month { get; set; }
+
+        public int Day { get; set; }
+
+        public string Surname { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string PoliceFileNumber { get; set; }
+
+        public int CourtFileNumber { get; set; }
+
+        public int SWCFileNumber { get; set; }
+
+        public List<RiskLevelModel> RiskLevel { get; set; }
+
+        public List<CrisisModel> Crisis { get; set; }
+
+        public List<ServiceModel> Service { get; set; }
+
+        public List<ProgramModel> Program { get; set; }
+
+        public string RiskAssessmentAssignedTo { get; set; }
+
+        public List<RiskStatusModel> RiskStatus { get; set; }
+
+        public List<AssignedWorkerModel> AssignedWorker { get; set; }
+
+        public List<ReferralSourceModel> ReferralSource { get; set; }
+
+        public List<ReferralContactModel> ReferralContact { get; set; }
+
+        public List<IncidentModel> Incident { get; set; }
+
+        public string AbuserName { get; set; }
+
+        public List<AbuserRelationshipModel> AbuserRelationship { get; set; }
+
+        public List<VictimOfIncidentModel> VictimOfIncident { get; set; }
+
+        public List<FamilyViolenceModel> FamilyViolenceFile { get; set; }
+
+        public char Gender { get; set; }
+
+        public List<EthnicityModel> Ethnicity { get; set; }
+
+        public List<AgeModel> Age { get; set; }
+
+        public List<RepeatClientModel> RepeatClient { get; set; }
+
+        public List<DuplicateFileModel> DuplicateFile { get; set; }
+
+        public int NumChildren0_6 { get; set; }
+
+        public int NumChildren7_12 { get; set; }
+
+        public int NumChildren13_18 { get; set; }
+
+        public List<StatusOfFileModel> StatusOfFile { get; set; }
+
+        public DateTime DateLastTransferred { get; set; }
+
+        public DateTime DateClosed { get; set; }
+
+        public DateTime DateReOpened { get; set; }
+    }
+}
---
+++ GoodSamaritan/Models/SmartModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GoodSamaritan.Models
+{
+    public class SmartModel
+    {
+        [ForeignKey('ClientModel')]
+        public int ClientReferenceNumber { get; set; }
+
+        public List<Work_EploitationModel> WorkExploitation { get; set; }
+
+        public List<MultiplePerpetratorsModel> MultiplePerpetrators { get; set; }
+
+        public List<DrugFaciliatedAssaultModel> DrugFaciliatedAssault { get; set; }
+
+        public List<CityOfAssaultModel> CityOfAssault { get; set; }
+
+        public List<CityOFResidenceModel> CityOfResidence { get; set; }
+
+        public int AccompanimentMinutes { get; set; }
+
+        public List<ReferralHospitalModel> ReferralHospital { get; set; }
+
+        public List<HospitalAttendedModel> HospitalAttended { get; set; }
+
+        public List<SocialWorkAttendanceModel> SocialWorkAttendance { get; set; }
+
+        public List<PoliceAttendanceModel> PoliceAttendance { get; set; }
+
+        public List<VictimServicesModel> VictimServices { get; set; }
+
+        public List<MdeicalOnlyModel> MedicalOnly { get; set; }
+
+        public List<EvidenceStoredModel> EvidenceStored { get; set; }
+
+        public List<HIVMedsModel> HIVMeds { get; set; }
+
+        public List<ReferredCBVSModel> ReferredToCBVS { get; set; }
+
+        public List<PoliceReportedModel> PoliceReported { get; set; }
+
+        public List<ThirdPartyReportModel> ThirdPartyReport { get; set; }
+
+        public List<BadDateReportModel> BadDateReport { get; set; }
+
+        public int NumberTransportsProvided { get; set; }
+
+        public bool ReferredToNursePractitioner { get; set; }
+    }
+}
