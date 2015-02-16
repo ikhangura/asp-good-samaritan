7c358c8a0742decbf21455f43821cd0e89dc3bf9
bensoer <bsoer@my.bcit.ca>
2015-02-15 17:47:09 -0800

    completed ClientEntity lookup table seeding

--- GoodSamaritan/Migrations/GoodSamaritanMigrations/Configuration.cs
+++ GoodSamaritan/Migrations/GoodSamaritanMigrations/Configuration.cs
@@ -98,7 +98,119 @@ namespace GoodSamaritan.Migrations.GoodSamaritanMigrations
                 new AssignedWorkerModel { AssignedWorker = "None" }
             );
 
+            context.ReferralSourceModel.AddOrUpdate(
+                rsm => rsm.ReferralSource,
+                new ReferralSourceModel { ReferralSource = "Community Agency" },
+                new ReferralSourceModel { ReferralSource = "Family/Friend" },
+                new ReferralSourceModel { ReferralSource = "Government" },
+                new ReferralSourceModel { ReferralSource = "CVAP" },
+                new ReferralSourceModel { ReferralSource = "CBVS" }
+            );
 
+            context.ReferralContactModel.AddOrUpdate(
+                rcm => rcm.ReferralContact,
+                new ReferralContactModel { ReferralContact = "PBVS" },
+                new ReferralContactModel { ReferralContact = "MCFD" },
+                new ReferralContactModel { ReferralContact = "VictimLINK" },
+                new ReferralContactModel { ReferralContact = "TH" },
+                new ReferralContactModel { ReferralContact = "Self" },
+                new ReferralContactModel { ReferralContact = "FNS" },
+                new ReferralContactModel { ReferralContact = "Other" },
+                new ReferralContactModel { ReferralContact = "Medical" }
+            );
+
+            context.IncidentModel.AddOrUpdate(
+                im => im.Incident,
+                new IncidentModel { Incident = "Abduction" },
+                new IncidentModel { Incident = "Adult Historical Secual Assault" },
+                new IncidentModel { Incident = "Adult Sexual Assault" },
+                new IncidentModel { Incident = "Partner Assault" },
+                new IncidentModel { Incident = "Attempted Murder" },
+                new IncidentModel { Incident = "Child Physical Assault" },
+                new IncidentModel { Incident = "Child Sexual Abuse/Exploitation" },
+                new IncidentModel { Incident = "Criminal Harrassment/Stalking" },
+                new IncidentModel { Incident = "Elder Abuse" },
+                new IncidentModel { Incident = "Human Trafficking" },
+                new IncidentModel { Incident = "N/A" },
+                new IncidentModel { Incident = "Other" },
+                new IncidentModel { Incident = "Other Assault" },
+                new IncidentModel { Incident = "Other Crime - DV" },
+                new IncidentModel { Incident = "Other Familial Assault" },
+                new IncidentModel { Incident = "Threatending" },
+                new IncidentModel { Incident = "Youth Secual Assault/Exploitation" }
+            );
+
+            context.AbuserRelationshipModel.AddOrUpdate(
+                arm => arm.AbuserRelationship,
+                new AbuserRelationshipModel { AbuserRelationship = "Acquaintance" },
+                new AbuserRelationshipModel { AbuserRelationship = "Bad Date" },
+                new AbuserRelationshipModel { AbuserRelationship = "DNA" },
+                new AbuserRelationshipModel { AbuserRelationship = "Ex-Partner" },
+                new AbuserRelationshipModel { AbuserRelationship = "Friend" },
+                new AbuserRelationshipModel { AbuserRelationship = "Multiple Perps" },
+                new AbuserRelationshipModel { AbuserRelationship = "N/A" },
+                new AbuserRelationshipModel { AbuserRelationship = "Other" },
+                new AbuserRelationshipModel { AbuserRelationship = "Other Familial" },
+                new AbuserRelationshipModel { AbuserRelationship = "Parent" },
+                new AbuserRelationshipModel { AbuserRelationship = "Partner" },
+                new AbuserRelationshipModel { AbuserRelationship = "Sibling" },
+                new AbuserRelationshipModel { AbuserRelationship = "Stranger" }
+            );
+
+            context.VictimOfIncidentModel.AddOrUpdate(
+                voim => voim.VictimOfIncident,
+                new VictimOfIncidentModel { VictimOfIncident = "Primary" },
+                new VictimOfIncidentModel { VictimOfIncident = "Secondary" }
+            );
+
+            context.FamilyViolenceModel.AddOrUpdate(
+                fvm => fvm.FamilyViolenceFile,
+                new FamilyViolenceModel { FamilyViolenceFile = "Yes" },
+                new FamilyViolenceModel { FamilyViolenceFile = "No" },
+                new FamilyViolenceModel { FamilyViolenceFile = "N/A" }
+            );
+
+            context.EthnicityModel.AddOrUpdate(
+                em => em.Ethnicity,
+                new EthnicityModel { Ethnicity = "Indigenous" },
+                new EthnicityModel { Ethnicity = "Asian" },
+                new EthnicityModel { Ethnicity = "Black" },
+                new EthnicityModel { Ethnicity = "Caucasian" },
+                new EthnicityModel { Ethnicity = "Declined" },
+                new EthnicityModel { Ethnicity = "Latin" },
+                new EthnicityModel { Ethnicity = "Middle Eastern" },
+                new EthnicityModel { Ethnicity = "Other" },
+                new EthnicityModel { Ethnicity = "South Asian" },
+                new EthnicityModel { Ethnicity = "South Eash Asian" }
+            );
+
+            context.AgeModel.AddOrUpdate(
+                am => am.Age,
+                new AgeModel { Age = "Adult(>24<65)" },
+                new AgeModel { Age = "Youth(>12<19)" },
+                new AgeModel { Age = "Youth(>18<25)" },
+                new AgeModel { Age = "Child(<13)" },
+                new AgeModel { Age = "Senior(>64)" }
+            );
+
+            context.RepeatClientModel.AddOrUpdate(
+                rcm => rcm.RepeatClient,
+                new RepeatClientModel { RepeatClient = "Yes" },
+                new RepeatClientModel { RepeatClient = "null" }
+            );
+
+            context.DuplicateFileModel.AddOrUpdate(
+                dfm => dfm.DuplicateFile,
+                new DuplicateFileModel { DuplicateFile = "Yes" },
+                new DuplicateFileModel { DuplicateFile = "null" }
+            );
+
+            context.StatusOfFileModel.AddOrUpdate(
+                sof => sof.StatusOfFile,
+                new StatusOfFileModel { StatusOfFile = "Reopened" },
+                new StatusOfFileModel { StatusOfFile = "Closed" },
+                new StatusOfFileModel { StatusOfFile = "Open" }
+            );
 
         }
     }
