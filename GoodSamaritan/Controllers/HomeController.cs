e417291daa9d7612f87161569799f97c5fe8648c
bensoer <bsoer@my.bcit.ca>
2015-03-01 14:50:27 -0800

    added logic to display menu when allowd

--- GoodSamaritan/Controllers/HomeController.cs
+++ GoodSamaritan/Controllers/HomeController.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Web.Security;
+
 
 namespace GoodSamaritan.Controllers
 {
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+                
             return View();
         }
 
         public ActionResult About()
         {
+
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -22,6 +28,7 @@ namespace GoodSamaritan.Controllers
 
         public ActionResult Contact()
         {
+           
             ViewBag.Message = "Your contact page.";
 
             return View();
