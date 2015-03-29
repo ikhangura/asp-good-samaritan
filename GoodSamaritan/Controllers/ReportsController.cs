07ca78e72969a96efb5037dd9cc51da337768007
bensoer <bsoer@my.bcit.ca>
2015-03-29 13:19:27 -0700

    did some cleanup and created reports view controller

--- GoodSamaritan/Controllers/ReportsController.cs
+++ GoodSamaritan/Controllers/ReportsController.cs
@@ -15,58 +15,46 @@ namespace GoodSamaritan.Controllers
 
         private GoodSamaritanModel db = new GoodSamaritanModel();
 
-        // GET: api/Reports
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Reports/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         [HttpPost]
         // POST: api/Reports
         public IHttpActionResult PostSearch([FromBody]ReportSearch search)
         {
 
             var numOpen = (from openStatus in db.ClientModel
-                          where openStatus.StatusOfFile.StatusOfFile == "Open"
-                          select openStatus).Count();
+                           where openStatus.StatusOfFile.StatusOfFile == "Open"
+                           select openStatus).Count();
             var numClosed = (from openStatus in db.ClientModel
-                           where openStatus.StatusOfFile.StatusOfFile == "Closed"
-                           select openStatus).Count();
+                             where openStatus.StatusOfFile.StatusOfFile == "Closed"
+                             select openStatus).Count();
             var numReOpened = (from openStatus in db.ClientModel
-                           where openStatus.StatusOfFile.StatusOfFile == "Reopened"
-                           select openStatus).Count();
+                               where openStatus.StatusOfFile.StatusOfFile == "Reopened"
+                               select openStatus).Count();
 
             var programCrisis = (from crisis in db.ClientModel
-                                where crisis.Program.Program == "Crisis"
-                                select crisis).Count();
+                                 where crisis.Program.Program == "Crisis"
+                                 select crisis).Count();
             var programCourt = (from crisis in db.ClientModel
-                                 where crisis.Program.Program == "Court"
-                                 select crisis).Count();
+                                where crisis.Program.Program == "Court"
+                                select crisis).Count();
             var programSmart = (from crisis in db.ClientModel
-                                 where crisis.Program.Program == "SMART"
-                                 select crisis).Count();
+                                where crisis.Program.Program == "SMART"
+                                select crisis).Count();
             var programDVU = (from crisis in db.ClientModel
-                                 where crisis.Program.Program == "DVU"
-                                 select crisis).Count();
+                              where crisis.Program.Program == "DVU"
+                              select crisis).Count();
             var programMCFD = (from crisis in db.ClientModel
-                                 where crisis.Program.Program == "MCFD"
-                                 select crisis).Count();
+                               where crisis.Program.Program == "MCFD"
+                               select crisis).Count();
 
             var genderMale = (from gender in db.ClientModel
-                                 where gender.Gender == "M"
-                                 select gender).Count();
+                              where gender.Gender == "M"
+                              select gender).Count();
             var genderFemale = (from gender in db.ClientModel
-                              where gender.Gender == "F"
-                              select gender).Count();
+                                where gender.Gender == "F"
+                                select gender).Count();
             var genderTrans = (from gender in db.ClientModel
-                              where gender.Gender == "Trans"
-                              select gender).Count();
+                               where gender.Gender == "Trans"
+                               select gender).Count();
 
             var ageAdult = (from age in db.ClientModel
                             where age.Age.Age == "Adult(>24<65)"
@@ -81,9 +69,9 @@ namespace GoodSamaritan.Controllers
                             where age.Age.Age == "Child(<13)"
                             select age).Count();
             var ageSenior = (from age in db.ClientModel
-                            where age.Age.Age == "Senior(>64)"
-                            select age).Count();
-            
+                             where age.Age.Age == "Senior(>64)"
+                             select age).Count();
+
 
 
             // put results into object
@@ -100,16 +88,6 @@ namespace GoodSamaritan.Controllers
             return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, success));
 
 
-        }
-
-        // PUT: api/Reports/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Reports/5
-        public void Delete(int id)
-        {
         }
     }
 }
---
+++ GoodSamaritan/Controllers/ReportsViewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodSamaritan.Controllers
+{
+    [Authorize(Roles="Administrator, Worker")]
+    public class ReportsViewController : Controller
+    {
+        // GET: ReportsView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
